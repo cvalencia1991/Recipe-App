5e928bed9613c10ac539cd7b64739c76de9afd31
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-07 17:20:42 -0500

    Add Recipe Show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,10 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+  end
 
   # GET /recipes/new
   def new
