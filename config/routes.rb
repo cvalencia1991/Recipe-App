26940d8fe0be7e28c802ca00b6c5194f7f83c50c
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-07 17:10:58 -0500

    Update the Form with the recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   root "users#index"
   resources :recipe_foods
-  resources :recipes
   resources :foods
-  resources :users
+  resources :users do
+    resources :recipes
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
