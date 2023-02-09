868597f4edf739a6ecc662e99d2dcaa9b4d4883d
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-09 11:22:05 -0500

    Fix Rubocop Linters

--- app/controllers/shopinglists_controller.rb
+++ app/controllers/shopinglists_controller.rb
@@ -1,5 +1,5 @@
 class ShopinglistsController < ApplicationController
-  def  index
+  def index
     @recipefoods = RecipeFood.includes(:food).where(recipe_id: params[:recipe_id])
   end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,6 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe, class_name: 'Recipe', foreign_key: 'recipe_id'
-  belongs_to :food,class_name: 'Food', foreign_key: 'food_id'
+  belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
 
   def inc_food_items
     Food.all.length
