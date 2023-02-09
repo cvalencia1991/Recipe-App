711cd99aac68de4f48130005dccd435cac941b0e
cvalencia1991 <cesar4a6z@gmail.com>
2023-02-09 11:44:08 -0500

    Fix Bug Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
+      user_params.permit(:email, :password)
     end
   end
 end
