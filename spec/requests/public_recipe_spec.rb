ec603957a843f7ee657d7293d42ce042d82159e4
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-02-09 02:43:11 +0200

    Fix linter error

--- spec/requests/public_recipe_spec.rb
+++ spec/requests/public_recipe_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "PublicRecipes", type: :request do
-  describe "GET /all" do
-    it "returns http success" do
-      get "/public_recipe/all"
+RSpec.describe 'PublicRecipes', type: :request do
+  describe 'GET /all' do
+    it 'returns http success' do
+      get '/public_recipe/all'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
