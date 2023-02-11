b0414c8e30e8ff4ddd1c8fd2644dca13ce5d092e
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-02-11 04:42:13 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   resources :users, only: %i[index] do
     resources :recipes do
-      resources :recipe_foods, only: %i[index new]
+      resources :recipe_foods, only: %i[index new create]
       resources :foods, only: %i[index show new create edit update destroy]
-      resources :shopinglists, only: %i[index]
+      resources :shopinglists, only: %i[index new create]
     end
   end
 end
